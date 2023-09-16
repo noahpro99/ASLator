cd68fe3335c7fef63e935be6a9a185bb41c9873d
noahpro <noahpro@gmail.com>
2023-09-16 10:18:00 -0400

    feat: ✨ change detection

--- frontend/src/pages/Hand.tsx
+++ frontend/src/pages/Hand.tsx
@@ -10,10 +10,18 @@ import {
 const Hand = () => {
   const [inputVideoReady, setInputVideoReady] = useState(false);
   const [loaded, setLoaded] = useState(false);
+  const [frameDiffState, setFrameDiffState] = useState(4);
+  const [changeThresholdState, setChangeThresholdState] = useState(4);
 
+  const frameDiff = useRef(4);
+  const changeThreshold = useRef(4);
   const inputVideoRef = useRef<HTMLVideoElement | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+  const prevLeftLandmarks = useRef<any | null>(null);
+  const prevRightLandmarks = useRef<any | null>(null);
+  const prevFrame = useRef<any | null>(null);
+  const currentFrame = useRef<any | null>(null);
 
   useEffect(() => {
     if (!inputVideoReady) {
@@ -97,10 +105,47 @@ const Hand = () => {
               return lerp(data.from!.z!, -0.15, 0.1, 10, 1);
             },
           });
+
+          // compare with previous results to detect if change
+          if (currentFrame.current - prevFrame.current > frameDiff.current) {
+            let totalDiff = 0;
+            prevFrame.current = currentFrame.current;
+            if (isRightHand) {
+              if (prevRightLandmarks.current) {
+                let diff = 0;
+                for (let i = 0; i < prevRightLandmarks.current.length; i++) {
+                  const prevLandmark = prevRightLandmarks.current[i];
+                  const landmark = landmarks[i];
+                  diff += Math.abs(prevLandmark.x - landmark.x);
+                  diff += Math.abs(prevLandmark.y - landmark.y);
+                  diff += Math.abs(prevLandmark.z - landmark.z);
+                }
+                totalDiff += diff;
+              }
+              prevRightLandmarks.current = landmarks;
+            } else {
+              if (prevLeftLandmarks.current) {
+                let diff = 0;
+                for (let i = 0; i < prevLeftLandmarks.current.length; i++) {
+                  const prevLandmark = prevLeftLandmarks.current[i];
+                  const landmark = landmarks[i];
+                  diff += Math.abs(prevLandmark.x - landmark.x);
+                  diff += Math.abs(prevLandmark.y - landmark.y);
+                  diff += Math.abs(prevLandmark.z - landmark.z);
+                }
+                totalDiff += diff;
+              }
+              prevLeftLandmarks.current = landmarks;
+            }
+            console.log(totalDiff);
+            if (totalDiff > changeThreshold.current) {
+              console.log('change');
+            }
+          }
+          currentFrame.current++;
         }
       }
-      contextRef.current.restore();
-    }
+    };
   };
 
   return (
@@ -120,7 +165,33 @@ const Hand = () => {
           inputVideoRef.current = el;
           setInputVideoReady(!!el);
         }}
-      />  
+      />
+      {/* input to change frameDiff */}
+      <input
+        type='number'
+        min={1}
+        max={30}
+        value={frameDiff.current}
+        onChange={(e) => {
+          frameDiff.current = parseInt(e.target.value);
+          setFrameDiffState(parseInt(e.target.value));
+        }}
+      />
+      {/* input to change changeThreshold */}
+      <input
+        type='number'
+        min={1}
+        max={30}
+        value={changeThreshold.current}
+        onChange={(e) => {
+          changeThreshold.current = parseInt(e.target.value);
+          setChangeThresholdState(parseInt(e.target.value));
+        }}
+      />
+      <div className='text-center'>
+        <div>frameDiff: {frameDiffState}</div>
+        <div>changeThreshold: {changeThresholdState}</div>
+      </div>
       <canvas
         className='mx-auto mt-10'
         ref={canvasRef} width={1280} height={720} />
